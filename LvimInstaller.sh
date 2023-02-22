d7c043c4a9c4d5743aadcee5957566c811ba49f2
E.J. Miller <42196469+gitphourked@users.noreply.github.com>
2023-02-21 18:10:22 -0800

    Add files via upload

---
+++ LvimInstaller.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#
+#  Lunarvim Installer With My Config
+#  * For apt distros 
+#
+#
+
+sudo apt install git make pip python npm node cargo
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+mv config.lua ~/.config/lvim/
